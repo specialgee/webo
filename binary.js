5dc5b1fed243ef974c4bf63b2e92c50b122dc98d
Glitch (webo-adventure) <none>
2020-06-22 23:08:09 +0000

    🗻👠 Checkpoint ./style.css:17922623/437 ./binary-world.html:17922623/4804 ./index.html:17922623/24 ./binary.js:17922623/6

--- binary.js
+++ binary.js
@@ -8,6 +8,6 @@ var digits = document.getElementsByClassName("digit");
 var h = setInterval(function() {
   for (var i = 0; i < digits.length; i++) {    
     digits[i].style.top = Math.floor(Math.random() * 200 + 1) + "px";
-    digits[i].style.left = Math.floor(Math.random() * 1000 + 1) + "px";
+    digits[i].style.left = Math.floor(Math.random() * 1900 + 1) + "px";
   }
 }, 400);
