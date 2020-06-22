2dfd6508c3d07be7f3fd1089810d44539c1fe43d
Glitch (webo-adventure) <none>
2020-06-22 22:33:05 +0000

    🍚🌁 Checkpoint ./script.js:17922623/2651 ./binary-world.html:17922623/1425 ./style.css:17922623/39

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+// prints "hello world" in the browser's dev tools console
+console.log("hello world");
+
+var digits = document.getElementsByClassName("digit");
+console.log(digits)
+
+var h = setInterval(function () {
+   //div_elem.style.top = Math.floor((Math.random() * 100) + 1)+"px"; 
+   //div_elem.style.left = Math.floor((Math.random() * 200) + 1)+"px";
+   //console.log('top = '+div_elem.style.top);
+   //console.log('left = '+div_elem.style.left); 
+  
+  for (var i = 0; i < digits.length; i++) {
+      console.log(digits[i])
+
+    digits[i].style.top = Math.floor((Math.random() * 100) + 1)+"px";
+    digits[i].style.left = Math.floor((Math.random() * 200) + 1)+"px";
+
+}
+
+}, 1000);
+
