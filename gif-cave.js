05efdafa2811a69b0933ee325cf5d9392eee2304
Glitch (webo-adventure) <none>
2020-06-23 00:03:32 +0000

    😅💑 Checkpoint ./gif-cave.js:17922623/707 ./style.css:17922623/4398 ./gif-cave.html:17922623/37

--- gif-cave.js
+++ gif-cave.js
@@ -8,19 +8,21 @@ var index = Math.floor(Math.random() * 4);
 function addGif(url) {
   var img = document.createElement("img");
 img.src = url;
+  
+  gif.innerHTML = "";
 gif.appendChild(img);
 }
 
 switch(index) {
   case 0:
-    addGif("https://media.giphy.com/media/g4hKza7HfxyOk/giphy.gif");
+    addGif("https://media.giphy.com/media/oY69d2bibMcFO/giphy.gif");
     break;
   case 1:
-    addGif("https://media.giphy.com/media/g4hKza7HfxyOk/giphy.gif");
+    addGif("https://media.giphy.com/media/69jy5FNbwGAZH8FSHN/giphy-downsized.gif");
   case 2:
-    addGif("https://media.giphy.com/media/g4hKza7HfxyOk/giphy.gif");
+    addGif("https://media.giphy.com/media/26BRE3wu5ZVxWOJLW/giphy-downsized.gif");
   case 3:
-    addGif("https://media.giphy.com/media/g4hKza7HfxyOk/giphy.gif");
+    addGif("https://media.giphy.com/media/KENOUMfISQiEGZdhGB/giphy-downsized.gif");
     break;
   default:
     addGif("https://media.giphy.com/media/g4hKza7HfxyOk/giphy.gif");
