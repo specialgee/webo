12b956d5c18fd00118090bbbcd4a1b821d063887
Glitch (webo-adventure) <none>
2020-06-23 00:14:49 +0000

    🐦🍶 Checkpoint ./gif-cave.js:17922623/3432 ./style.css:17922623/6

--- gif-cave.js
+++ gif-cave.js
@@ -1,29 +1,35 @@
 // Affiche un gif aléatoirement
 
 var gif = document.getElementById("gif");
-gif.innerHTML = "";
 
-var index = Math.floor(Math.random() * 4);
+var index = Math.floor(Math.random()*4)+1;
+console.log("GIF: ", index);
 
 function addGif(url) {
   var img = document.createElement("img");
-img.src = url;
-  
+  img.src = url;
+
   gif.innerHTML = "";
-gif.appendChild(img);
+  gif.appendChild(img);
 }
 
-switch(index) {
+switch (index) {
   case 0:
     addGif("https://media.giphy.com/media/oY69d2bibMcFO/giphy.gif");
     break;
   case 1:
-    addGif("https://media.giphy.com/media/69jy5FNbwGAZH8FSHN/giphy-downsized.gif");
+    addGif(
+      "https://media.giphy.com/media/dtsxqRt0VdZVioQcDh/giphy.gif"
+    );
   case 2:
-    addGif("https://media.giphy.com/media/26BRE3wu5ZVxWOJLW/giphy-downsized.gif");
+    addGif(
+      "https://media.giphy.com/media/26BRE3wu5ZVxWOJLW/giphy-downsized.gif"
+    );
   case 3:
-    addGif("https://media.giphy.com/media/KENOUMfISQiEGZdhGB/giphy-downsized.gif");
+    addGif(
+      "https://media.giphy.com/media/KENOUMfISQiEGZdhGB/giphy-downsized.gif"
+    );
     break;
   default:
-    addGif("https://media.giphy.com/media/g4hKza7HfxyOk/giphy.gif");
+    addGif("https://media.giphy.com/media/u47uzYbZu3r535TmGr/giphy.gif");
 }
